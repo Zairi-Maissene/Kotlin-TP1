2ccc35c0027b295c3743acc88adf85fbb001ed0b
Maissene Zairi <maissen.zairi@gmail.com>
2023-10-05 01:19:41 +0100

    tp1 ex1

--- app/src/main/java/com/example/tp1/MainActivity.kt
+++ app/src/main/java/com/example/tp1/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.example.tp1
 
-import android.graphics.PorterDuff
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tp1.ui.theme.TP1Theme
+import android.view.Gravity
+import android.view.ViewGroup
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,23 +17,24 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         val myButton = findViewById<Button>(R.id.button)
-        val myInput = findViewById<EditText>(R.id.input)
+        val email = findViewById<EditText>(R.id.input)
+        val password = findViewById<EditText>(R.id.password_input)
 
         val defaultBackgroundColor = myButton.background // Store the default background drawable
 
         myButton.setOnClickListener { view ->
-                // Change the background color when the Button gains focus
-                myButton.background = getDrawable(R.color.purple_200);
-                     // Set your focused color here
+            // Change the background color when the Button gains focus
+            myButton.background = getDrawable(R.color.purple_200);
+            // Set your focused color here
+            //get the Edittext value
+            val emailText = email.getText().toString();
+            val passwordText = password.getText().toString();
+            val toastMsg = "Email: $emailText\nPassword: $passwordText"
+
+            //Display toast:
+            Toast.makeText(this, toastMsg, Toast.LENGTH_SHORT).show() ;
+
+
         }
-        myInput.setOnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) {
-                // Change the background color when the Button gains focus
-                myInput.background = getDrawable(R.color.purple_200);
-            } else {
-                // Restore the default background color when focus is lost
-                myInput.background = defaultBackgroundColor
-            }
-        }
-        }
+    }
 }
